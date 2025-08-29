007b03071235c4c3815adfc96fd8578246bbe40f
JavierBrizuela <jbrizuela1976@gmail.com>
2025-08-29 17:45:27 -0300

    feat:FractionalBasket

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 from backtesting import Strategy, Backtest
-from backtesting.lib import crossover
+from backtesting.lib import crossover, FractionalBacktest
 import talib
 
 symbol = "BTC-USD"
@@ -24,28 +24,31 @@ if isinstance(data.columns, pd.MultiIndex):
     
     data.columns = new_columns
 
+def SMA(values, n):
+   
+    return pd.Series(values).rolling(n).mean()
+
 class MyStrategy(Strategy):
-    n1 = 10
+    n1 = 5
     n2 = 20
     def init(self):
-        self.sma1 = self.I(talib.SMA, self.data['Close'], timeperiod=self.n1)
-        self.sma2 = self.I(talib.SMA, self.data['Close'], timeperiod=self.n2)
+        self.sma1 = self.I(SMA, self.data.Close, self.n1)
+        self.sma2 = self.I(SMA, self.data.Close, self.n2)
 
     def next(self):
-        if crossover(self.sma1, self.sma2):
+        if crossover(self.sma1, self.sma2) and not self.position.is_long:
             self.position.close()
             self.buy()
-        elif crossover(self.sma2, self.sma1):
+        elif crossover(self.sma2, self.sma1) and self.position.is_long:
             self.position.close()
-            self.sell()
 
-bt = Backtest(data, MyStrategy, cash=10000, commission=.002)
+bt = FractionalBacktest(data, MyStrategy, cash=20000, commission=.002, finalize_trades=True, fractional_unit=1e-08)
 stats = bt.run()
 print(stats)
 bt.plot()
-stats = bt.optimize(
-    n1=(5, 50, 5),
+""" stats = bt.optimize(
+    n1=(5, 20,5),
     n2=(20, 200, 5),
     maximize = "Return [%]"
-)
-print(stats._strategy)
+) """
+print(stats._trades)
